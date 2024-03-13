70393c47219f4a3ed54e0cb513d512e6ef5a2b93
mnasrullah0106 <nasirminhas36@gmail.com>
2024-03-13 11:16:26 -0700

    feature data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import ProductList from './components/ProductCard';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Hello E-Commerce</h1>
+     {/* <h1>Hello E-Commerce</h1> */}
+     <ProductList />
     </>
   )
 }
 
-export default App
+export default App;
